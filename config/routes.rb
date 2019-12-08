f41ec89e2380cef28f8a654b88548d8d9a15dab9
kirikirio <kirikiri3926@gmail.com>
2019-12-08 13:00:46 +0900

    初期設定　route コントローラーモデル周辺

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'manual/index'
+  resources :manual, only: [:index, :show, :new, :edit] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
