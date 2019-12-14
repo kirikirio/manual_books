86c6b2f9f02fde3851c879c5964a35efb524ec93
kirikirio <kirikiri3926@gmail.com>
2019-12-14 19:30:28 +0900

    コントローラー周り、検索結果ページを用意

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
   get 'manuals/top'
   get 'manuals/flyer'
   get 'manuals/show'
-  get '/show', to: 'manuals#show'
-  post '/show', to: 'manuals#show'
+  # get '/show', to: 'manuals#show'
+  # post '/show', to: 'manuals#show'
+  get '/list', to: 'manuals#list'
+  post '/list', to: 'manuals#list'
   # get 'manual/index'
   # # get 'manual/new'
   # post 'manual/create'
-  resources :manuals, only: [:index, :new, :create] 
+  resources :manuals, only: [:index, :new, :create,:list] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
